5062c0ac9a127f0e268d1b20b07958a4d008a7b4
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-10-03 10:44:20 +0200

    Add proper output pins for processor

--- hardware/Processor.vhd
+++ hardware/Processor.vhd
@@ -6,12 +6,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Processor is
     Port ( -- SRAM
            sram_1_address_out : out  STD_LOGIC_VECTOR (18 downto 0);
+           sram_1_control_out : out STD_LOGIC_VECTOR (2 downto 0);
            sram_1_data_inout : inout  STD_LOGIC_VECTOR (15 downto 0);
+
+           sram_2_control_out : out STD_LOGIC_VECTOR (2 downto 0);
            sram_2_address_out : out  STD_LOGIC_VECTOR (18 downto 0);
            sram_2_data_inout : inout  STD_LOGIC_VECTOR (15 downto 0);
+
+           -- HDMI && VGA
+           hdmi_out : out STD_LOGIC_VECTOR (18 downto 0);
+           vga_out : out STD_LOGIC_VECTOR (15 downto 0);
+
+           -- MC
+           mc_ebi_bus : inout STD_LOGIC_VECTOR (49 downto 0);
+           mc_spi_bus : inout STD_LOGIC_VECTOR (4 downto 0);
            
-           -- MPU
-           mpu_data_inout : inout  STD_LOGIC_VECTOR (19 downto 0));
+           -- Generic IO
+           led_1_out : out STD_LOGIC;
+           led_2_out : out STD_LOGIC);
 end Processor;
 
 architecture Behavioral of Processor is
