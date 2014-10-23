0b59925931d712786c5fa9db5d7ef21f61d8b683
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-10-23 10:41:20 +0200

    Extend defines with sp_memory types

--- common/defines.vhd
+++ common/defines.vhd
@@ -3,10 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package defines is
 
-	constant INSTRUCTION_ADDRESS_WIDTH: integer := 16;
-	constant DATA_WIDTH: integer := 19;
-	constant WORD_WIDTH: integer := 16;
-	
+  constant INSTRUCTION_ADDRESS_WIDTH: integer := 16;
+  constant DATA_ADDRESS_WIDTH: integer := 19;
+  constant DATA_WIDTH: integer := 19;
+  constant WORD_WIDTH: integer := 16;
+
   type sram_bus_control_t is
     record
       address : std_logic_vector(18 downto 0);
@@ -23,7 +24,9 @@ package defines is
   subtype word_t is std_logic_vector(WORD_WIDTH -1 downto 0);
   subtype ebi_bus_t is std_logic_vector(49 downto 0);
   subtype spi_bus_t is std_logic_vector(4 downto 0);
-	subtype instruction_address_t is std_logic_vector(INSTRUCTION_ADDRESS_WIDTH -1 downto 0);
-	subtype thread_id_t is std_logic_vector(DATA_WIDTH -1 downto 0);
-	
+  subtype instruction_address_t is std_logic_vector(INSTRUCTION_ADDRESS_WIDTH -1 downto 0);
+  subtype thread_id_t is std_logic_vector(DATA_WIDTH -1 downto 0);
+
+  type sp_memory_addresses_t is array(NUMBER_OF_STREAMING_PROCESSORS - 1 downto 0) of instruction_address_t;
+
 end package defines;
