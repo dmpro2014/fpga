c7631b72febba3d128c63e38d51b6f7636f2d22b
Eirik Jakobsen <eirikjak@gmail.com>
2014-10-24 11:36:52 +0200

    reset signal for pc

--- hardware/pc.vhd
+++ hardware/pc.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity pc is
     Port ( clk : in std_logic;
+           reset: in std_logic;
            write_enable : in std_logic;
            pc_in : in std_logic_vector (15 downto 0);
 					 pc_input_select_in: in std_logic;
