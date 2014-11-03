f88de81c987bf91024fd08ed399ee7731f702855
Eirik Jakobsen <eirikjak@gmail.com>
2014-11-03 15:53:30 +0100

    Test for register file

--- testbench/tb_register_file.vhd
+++ testbench/tb_register_file.vhd
@@ -1,16 +1,16 @@
-
-  
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.defines.all;
 use work.test_utils.all;
+
 entity tb_register_file is
 end tb_register_file;
 
 architecture behavior of tb_register_file is 
 
-  constant reg_addr_bits: integer := 3;
+  constant reg_addr_bits: integer := 4;
+  constant num_registers: integer := 16;
   -- General registers
   signal clk: std_logic;
   signal read_register_1_in: std_logic_vector(reg_addr_bits -1 downto 0);
@@ -52,7 +52,7 @@ architecture behavior of tb_register_file is
 -- component instantiation
         register_file: entity work.register_file
         generic map(
-              DEPTH => 8,
+              DEPTH => num_registers,
               LOG_DEPTH => reg_addr_bits
         )
         port map(
@@ -117,7 +117,7 @@ architecture behavior of tb_register_file is
       write_register_in <= get_reg_addr(2);
       wait for clk_period;
       assert_equals(make_word(7), read_data_1_out, "ID should be readonly.");
-      assert_equals(make_word(7), read_data_2_out, "ID should be readonly.");
+      assert_equals("1111111111111111", read_data_2_out, "ID should be readonly.");
         
       -- Address high/low can be treaded as general purpose registers.
       -- Only difference is that their out should also be in lsu_data.
@@ -133,16 +133,45 @@ architecture behavior of tb_register_file is
       write_register_in <= get_reg_addr(4);
       -- Write to both registers
       wait for clk_period;
-      assert_equals(write_data_in, read_register_1_in, "Should be treaded as a general purpose register.");
-      assert_equals(write_data_in, read_register_2_in, "Should be treaded as a general purpose register.");
+      assert_equals(write_data_in, read_data_1_out, "Should be treated as a general purpose register.");
+      assert_equals(write_data_in, read_data_2_out, "Should be treated as a general purpose register.");
       -- Test special feature
       assert_equals(ALL_BITS_HIGH, lsu_address_out, "LSU address should consist of Address low and high bits from address high."); 
-  
+    
+     --Return register($5) is also a general purpose register
+     --Test general purpose first
+     read_register_1_in <= get_reg_addr(5);
+     write_register_in <= get_reg_addr(5);
+     write_data_in <= make_word(23648912);
+     
+     wait for clk_period;
+     assert_equals(make_word(23648912), read_data_1_out, "Should be treated as a general purpose register..");
+     --Test write from lsu
+     write_register_in <= get_reg_addr(0);
+     register_write_enable_in <= '0';
+     return_register_write_enable_in <= '1';
+     lsu_data_inout <= make_word(9);
+     wait for clk_period;
+     assert_equals(make_word(9), read_data_1_out, "LSU should be able to write result");
       
-      
-      
-      
-
+     --Mask register
+     read_register_1_in <= get_reg_addr(6);
+     register_write_enable_in <= '1';
+     write_data_in <= make_word(1);
+     wait for clk_period;
+     assert_equals('1', predicate_out, "Predicate should be writable.");
+     
+     register_write_enable_in <= '1';
+     --Test other general registers
+      for i in 6 to num_registers loop
+        read_register_1_in <= get_reg_addr(i);
+        read_register_2_in <= get_reg_addr(i);
+        write_register_in <= get_reg_addr(i);
+        write_data_in <= make_word(30 + i);
+        wait for clk_period;
+        assert_equals(make_word(30 + i), read_data_1_out, "Should be able to read/write general purpose register.");
+        assert_equals(make_word(30 + i), read_data_2_out, "Should be able to read/write general purpose register.");
+      end loop;
       wait; -- will wait forever
    end process tb;
   --  end test bench 
