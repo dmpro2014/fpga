fbfbbb9cc086f0dcb203135a13d801ff60f60d84
Aleksander Wasaznik <aleksander@wasaznik.com>
2014-11-06 17:35:36 +0100

    Correct timing issues in mem_control.

--- hardware/lsu/mem_control.vhd
+++ hardware/lsu/mem_control.vhd
@@ -7,43 +7,44 @@ entity mem_control is
 
     port
         ( clock          : in    std_logic
+
+        ; request_packet : in    request_t
+        ; write_enable   : in    std_logic
+        ; read_response  : out   read_response_t
+
         ; ram_control    : out   sram_bus_control_t
         ; ram_data       : inout sram_bus_data_t
-        ; request_packet : in    request_t
-        ; read_response  : out   read_response_t
         );
 
 end mem_control;
 
-architecture Behavioral of mem_control is
+-- On clock-rise, latch a request. If it's valid, let it go
+-- to ram. Output the response from ram. The reponse is
+-- combinatoric, and valid only till the next cycle.
+architecture Behavioral of mem_control is begin
 
-    signal delayed_valid       : std_logic     := '0';
-    signal delayed_barrel_line : barrel_row_t;
-    signal delayed_sp          : sp_number;
-
-begin
+    -- Output the response from ram immediately.
+    read_response.data <= ram_data.data;
 
     process (clock) begin
         if rising_edge(clock) then
 
+            -- Setup ram-bus. Write_enable is active-low.
             ram_control.address      <= request_packet.address;
-            ram_control.write_enable <= request_packet.write_enable;
+            ram_control.write_enable <= not (request_packet.valid and write_enable);
 
-            delayed_valid       <= request_packet.valid;
-            delayed_barrel_line <= request_packet.barrel_line;
-            delayed_sp          <= request_packet.sp;
-
-            read_response.valid       <= delayed_valid;
-            read_response.barrel_line <= delayed_barrel_line;
-            read_response.sp          <= delayed_sp;
-
-            if request_packet.write_enable = '1' then
+            -- Handle the half-duplex data-bus to ram.
+            -- When write_enable is low, disconnect it from load using a TBUF.
+            if write_enable = '1' then
                 ram_data.data <= request_packet.write_data;
             else
                 ram_data.data <= (others => 'Z');
             end if;
 
-            read_response.data <= ram_data.data;
+
+            -- Outputs to response-handler.
+            read_response.sp    <= request_packet.sp;
+            read_response.valid <= request_packet.valid and not write_enable;
 
         end if;
     end process;
