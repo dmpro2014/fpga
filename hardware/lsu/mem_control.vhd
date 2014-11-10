a5d1f3275b10cab587158612fa61a29839473946
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2014-11-10 02:10:23 +0100

    Revert the change to mem_control. It was only for testing.

--- hardware/lsu/mem_control.vhd
+++ hardware/lsu/mem_control.vhd
@@ -21,15 +21,10 @@ end mem_control;
 -- On clock-rise, latch a request. If it's valid, let it go
 -- to ram. Output the response from ram. The reponse is
 -- combinatoric, and valid only till the next cycle.
-architecture Behavioral of mem_control is
-
-    signal write_data : std_logic_vector(15 downto 0) := (others => 'Z');
-
-begin
+architecture Behavioral of mem_control is begin
 
     -- Output the response from ram immediately.
     read_response.data <= ram_data.data;
-    ram_data.data <= write_data;
 
     process (clock) begin
         if rising_edge(clock) then
@@ -41,9 +36,9 @@ begin
             -- Handle the half-duplex data-bus to ram.
             -- When write_enable is low, disconnect it from load using a BUFT.
             if write_enable = '1' then
-                write_data <= request_packet.write_data;
+                ram_data.data <= request_packet.write_data;
             else
-                write_data <= (others => 'Z');
+                ram_data.data <= (others => 'Z');
             end if;
 
 
