4ef81e562075c2ab5f58e1db4e39576302d291da
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-11-15 21:58:55 +0100

    Allow for single-pulse threadspawner enable

--- hardware/thread_spawner.vhd
+++ hardware/thread_spawner.vhd
@@ -2,21 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.defines.all;
 use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_misc.all;
 
 entity thread_spawner is
-    Port ( clk : in std_logic;
-					thread_done_in: in std_logic;
-					--Engage signal
-					kernel_start_in: in std_logic;
-					kernel_addr_in: in instruction_address_t;
-					kernel_complete_out: out std_logic;
-					
-					num_threads_in: in thread_id_t;
-					pc_start_out: out instruction_address_t;
-					pc_input_select_out: out std_logic;
-					thread_id_out : out thread_id_t;
-					id_write_enable_out: out std_logic);
-			
+  Port ( clk : in std_logic;
+         thread_done_in: in std_logic;
+          --Engage signal
+         kernel_start_in: in std_logic;
+         kernel_addr_in: in instruction_address_t;
+         kernel_complete_out: out std_logic;
+
+         num_threads_in: in thread_id_t;
+         pc_start_out: out instruction_address_t;
+         pc_input_select_out: out std_logic;
+         thread_id_out : out thread_id_t;
+         id_write_enable_out: out std_logic);
+
 end thread_spawner;
 
 architecture Behavioral of thread_spawner is
@@ -29,11 +30,13 @@ architecture Behavioral of thread_spawner is
   signal update_next_id   : std_logic;
   signal last_spawned     : std_logic;
   signal spawn_new_threads: std_logic;
-  signal kernel_start_in_prev : std_logic := '0';
 
+  -- Delaytrain signals
+  signal delaytrain : std_logic_vector(0 to (BARREL_HEIGHT-1)) := (others => '0');
+  signal kernel_start_in_prev : std_logic := '0';
 begin
 
-  update_next_id <= kernel_start_in or thread_done_in;
+  update_next_id <= or_reduce(delaytrain) or thread_done_in;
   
   pc_start_out <= kernel_addr_reg;
   
@@ -72,11 +75,20 @@ begin
   begin
     if rising_edge(clk) then
       next_id_reg <= next_id_in;
+
+      -- Reset on first edge rise
+      delaytrain(0) <= kernel_start_in and not kernel_start_in_prev;
       if kernel_start_in_prev = '0' and kernel_start_in = '1' then
         next_id_reg <= (others => '0');
       end if;
+
+      -- Shift the delay train
+      for i in 1 to (BARREL_HEIGHT-1) loop
+        delaytrain(i) <= delaytrain(i-1);
+      end loop;
+
       kernel_start_in_prev <= kernel_start_in;
     end if;
   end process;
-  
+
 end Behavioral;
