5daf3f690381665557a91e9b69fceb395d8cd288
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-11-15 23:48:05 +0100

    Use entity instead of dbl-declaration in tb_system

--- testbench/tb_system.vhd
+++ testbench/tb_system.vhd
@@ -3,36 +3,11 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE work.defines.all;
 USE work.test_utils.all;
- 
+
 ENTITY tb_system IS
 END tb_system;
- 
+
 ARCHITECTURE behavior OF tb_system IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT System
-    PORT(
-         clk : IN  std_logic;
-         reset : IN  std_logic;
-         sram_bus_data_1_inout : INOUT  sram_bus_data_t;
-         sram_bus_control_1_out : OUT  sram_bus_control_t;
-         sram_bus_data_2_inout : INOUT  sram_bus_data_t;
-         sram_bus_control_2_out : OUT  sram_bus_control_t;
-         hdmi_bus_data_inout : INOUT  sram_bus_data_t;
-         hdmi_bus_control_in : IN  sram_bus_control_t;
-         vga_bus_data_inout : INOUT  sram_bus_data_t;
-         vga_bus_control_in : IN  sram_bus_control_t;
-         ebi_data_inout : INOUT  ebi_data_t;
-         ebi_control_in : IN  ebi_control_t;
-         mc_kernel_complete_out : OUT  std_logic;
-         mc_sram_flip_in : IN  std_logic;
-         mc_spi_bus : INOUT  spi_bus_t;
-         led_1_out : OUT  std_logic;
-         led_2_out : OUT  std_logic
-        );
-    END COMPONENT;
-    
 
    --Inputs
    signal clk : std_logic := '0';
@@ -42,7 +17,7 @@ ARCHITECTURE behavior OF tb_system IS
    signal ebi_control_in : ebi_control_t;
    signal mc_sram_flip_in : std_logic := '0';
 
-	--BiDirs
+   --BiDirs
    signal sram_bus_data_1_inout : sram_bus_data_t;
    signal sram_bus_data_2_inout : sram_bus_data_t;
    signal hdmi_bus_data_inout : sram_bus_data_t;
@@ -50,7 +25,7 @@ ARCHITECTURE behavior OF tb_system IS
    signal ebi_data_inout : ebi_data_t;
    signal mc_spi_bus : spi_bus_t;
 
- 	--Outputs
+   --Outputs
    signal sram_bus_control_1_out : sram_bus_control_t;
    signal sram_bus_control_2_out : sram_bus_control_t;
    signal mc_kernel_complete_out : std_logic;
@@ -66,27 +41,28 @@ ARCHITECTURE behavior OF tb_system IS
    signal sram_b : mem_t := (others => (others => '0'));
 
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: System PORT MAP (
-          clk => clk,
-          reset => reset,
-          sram_bus_data_1_inout => sram_bus_data_1_inout,
-          sram_bus_control_1_out => sram_bus_control_1_out,
-          sram_bus_data_2_inout => sram_bus_data_2_inout,
-          sram_bus_control_2_out => sram_bus_control_2_out,
-          hdmi_bus_data_inout => hdmi_bus_data_inout,
-          hdmi_bus_control_in => hdmi_bus_control_in,
-          vga_bus_data_inout => vga_bus_data_inout,
-          vga_bus_control_in => vga_bus_control_in,
-          ebi_data_inout => ebi_data_inout,
-          ebi_control_in => ebi_control_in,
-          mc_kernel_complete_out => mc_kernel_complete_out,
-          mc_sram_flip_in => mc_sram_flip_in,
-          mc_spi_bus => mc_spi_bus,
-          led_1_out => led_1_out,
-          led_2_out => led_2_out
-        );
+
+  -- Instantiate the Unit Under Test (UUT)
+  uut: entity work.System
+  PORT MAP (
+             clk => clk,
+             reset => reset,
+             sram_bus_data_1_inout => sram_bus_data_1_inout,
+             sram_bus_control_1_out => sram_bus_control_1_out,
+             sram_bus_data_2_inout => sram_bus_data_2_inout,
+             sram_bus_control_2_out => sram_bus_control_2_out,
+             hdmi_bus_data_inout => hdmi_bus_data_inout,
+             hdmi_bus_control_in => hdmi_bus_control_in,
+             vga_bus_data_inout => vga_bus_data_inout,
+             vga_bus_control_in => vga_bus_control_in,
+             ebi_data_inout => ebi_data_inout,
+             ebi_control_in => ebi_control_in,
+             mc_kernel_complete_out => mc_kernel_complete_out,
+             mc_sram_flip_in => mc_sram_flip_in,
+             mc_spi_bus => mc_spi_bus,
+             led_1_out => led_1_out,
+             led_2_out => led_2_out
+           );
 
    -- Clock process definitions
    clk_process :process
